a1cac534abb67af874b72229773e8f0255944fad
luugiahuy123 <luugiahuyqn@gmail.com>
2024-07-04 15:31:50 +0700

    huyUpFe4/7

--- Front_end/src/layout/Sidebar/Sidebar.js
+++ Front_end/src/layout/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 export function Sidebar(props) {
-    return(
+    return (
         <>
             <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
