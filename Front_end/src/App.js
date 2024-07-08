1bb36cc3e8d4095c5f190ee42318f46ade1f0cae
C0823G1-nguyentrucvi <nguyentrucvi10112004@gmail.com>
2024-07-08 11:33:00 +0700

    truc vi

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Master from "./pages/Master/Master";
-import './App.css';
-import {AllList} from "./components/List/AllList";
+import "./App.css";
+import { AllList } from "./components/List/AllList";
+import { DetailSong } from "./components/List/SongList/DetailSong";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Master/>}>
-                    <Route path="/" element={<AllList/>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Master />}>
+          <Route path="/" element={<AllList />} />
+          <Route path="/detail/:id" element={<DetailSong />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ Front_end/src/components/List/SongList/DetailSong.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Figure from "react-bootstrap/Figure";
+
+import * as SongsService from "../../../service/SongsService";
+export const DetailSong = () => {
+  const [songs, setSongs] = useState([]);
+
+  const getSongs = async (id) => {
+    try {
+      console.log(`current id: ${id}`);
+      const res = await SongsService.findByID(id);
+      setSongs(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const handleDetail = async (value, id) => {
+    const res = await SongsService.detailSongs(value, id);
+  };
+  //   useEffect(() => {
+
+  //     console.log(`staff: ${songs}`);
+  //     console.log(id);
+  //   },[id, staff]);
+  if (!songs) {
+    return <div>loanding...</div>;
+  }
+  return (
+    <div>
+      <Figure>
+        <div>
+          <Figure.Image
+            width={171}
+            height={180}
+            alt="171x180"
+            src="https://th.bing.com/th/id/R.78ae726a1bc19dc056b3bb5d3439f298?rik=Au1PZhb1kRIDOQ&pid=ImgRaw&r=0"
+          />
+          <Figure.Caption>
+            Nulla vitae elit libero, a pharetra augue mollis interdum.
+          </Figure.Caption>
+        </div>
+      </Figure>
+      <div className="media-body">
+        <h5 className="mt-0">Don't</h5>
+        <p className="mb-1">
+          <a href="/nghe-si/Lee-Chae-Yeon" className="text-secondary">
+            LEE CHAE YEON Thông Tin Số bài hát Ngày phát hành Cung cấp bởi 1
+            03/07/2024 TECHBEAT Music
+          </a>
+        </p>
+        <p className="text-muted">
+          <small>Hôm qua</small>
+        </p>
+      </div>
+      <div className="media-right">
+        <button className="btn btn-outline-primary btn-sm">
+          <i className="icon ic-like"></i> Like
+        </button>
+        <button className="btn btn-outline-secondary btn-sm">
+          <i className="icon ic-more"></i> More
+        </button>
+        <span className="duration">02:51</span>
+      </div>
+    </div>
+  );
+};
