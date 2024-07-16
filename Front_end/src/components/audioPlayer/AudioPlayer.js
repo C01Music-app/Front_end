db357b393d5abeb87fee2d7f6134dd731936a5f7
MAC <mac@macpro.local>
2024-07-16 08:27:51 +0700

    pull 8:27

--- Front_end/src/components/audioPlayer/AudioPlayer.js
+++ Front_end/src/components/audioPlayer/AudioPlayer.js
@@ -1,13 +1,8 @@
-
 import "./AudioPlayer.css"
-import PrevIcon from "./icons/PrevIcon";
-import NextIcon from "./icons/NextIcon";
-import PauseIcon from "./icons/PauseIcon";
-import PlayIcon from "./icons/PlayIcon";
+import { FaPlay, FaPause, FaForward, FaBackward, FaRandom, FaRedo } from 'react-icons/fa';
 import audios from "../../audios"
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import TimeSlider from "react-input-slider";
-
 
 const AudioPlayer = () => {
     const audioRef = useRef();
@@ -15,6 +10,8 @@ const AudioPlayer = () => {
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
     const [isPlay, setPlay] = useState(false);
+    const [isRandom, setRandom] = useState(false);
+    const [isRedo, setRedo] = useState(false);
 
     const handleLoadedData = () => {
         setDuration(audioRef.current.duration);
@@ -39,68 +36,110 @@ const AudioPlayer = () => {
             audioRef.current.play();
         }
     };
-    const handleSongs = () =>{
-        setAudioIndex((prevIndex) => {
-            const nextIndex = (prevIndex + 1) % audios.length;
-            return nextIndex;
-        });
+
+    const handleSongs = () => {
+        if (isRedo) {
+            audioRef.current.play();
+        } else if (isRandom) {
+            setAudioIndex(Math.floor(Math.random() * audios.length));
+        } else {
+            setAudioIndex((prevIndex) => {
+                const nextIndex = (prevIndex + 1) % audios.length;
+                return nextIndex;
+            });
+        }
     };
+
+    const handleRandomClick = () => {
+        setRandom(!isRandom);
+        if (isRedo) setRedo(false); // Tắt Redo nếu Random được bật
+    };
+
+    const handleRedoClick = () => {
+        setRedo(!isRedo);
+        if (isRandom) setRandom(false); // Tắt Random nếu Redo được bật
+    };
+
     React.useEffect(() => {
         if (audioRef.current) {
             audioRef.current.play();
         }
     }, [audioIndex]);
 
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
+
     return (
         <div className="AudioPlayer">
-            <div className="song-info">
-            <img className="Song-Thumbnail" src="img/images.png" alt="tet" />
-            <h2 className="Song-Title">{audios[audioIndex].title}</h2>
-            <p className="Singer">{audios[audioIndex].artist}</p>
-            </div>
-            <div className="song-control">
-            <div className="Control-Button-Group">
-                <div
-                    className="Prev-Button"
-                    onClick={() => setAudioIndex((audioIndex - 1) % audios.length)}
-                >
-                    <PrevIcon />
+            <div className="container">
+                <div className="song-info">
+                    {/*<img className="Song-Thumbnail" src="img/images.png" alt="tet" />*/}
+                    <h2 className="Song-Title">{audios[audioIndex].title}</h2>
+                    <p className="Singer">{audios[audioIndex].artist}</p>
                 </div>
-                <div className="Pause-Play-Button" onClick={handlePausePlayClick}>
-                    {isPlay ? <PauseIcon /> : <PlayIcon />}
+                <div className="song-control">
+                    <div className="Control-Button-Group">
+                        <div
+                            className={`Random-Button ${isRandom ? "active" : ""}`}
+                            onClick={handleRandomClick}
+                        >
+                            <FaRandom />
+                        </div>
+                        <div
+                            className="Prev-Button"
+                            onClick={() => setAudioIndex((audioIndex - 1 + audios.length) % audios.length)}
+                        >
+                            <FaBackward />
+                        </div>
+                        <div className="Pause-Play-Button" onClick={handlePausePlayClick}>
+                            {isPlay ? <FaPause /> : <FaPlay />}
+                        </div>
+                        <div
+                            className="Next-Button"
+                            onClick={() => setAudioIndex((audioIndex + 1) % audios.length)}
+                        >
+                            <FaForward />
+                        </div>
+                        <div
+                            className={`Redo-Button ${isRedo ? "active" : ""}`}
+                            onClick={handleRedoClick}
+                        >
+                            <FaRedo />
+                        </div>
+                    </div>
+                    <div className="timeSliderCustom">
+                        <div className="time-display">
+                            <span className="time1">{formatTime(currentTime)}</span>
+                            <TimeSlider
+                                axis="x"
+                                xmax={duration}
+                                x={currentTime}
+                                onChange={handleTimeSliderChange}
+                                styles={{
+                                    track: {
+                                        backgroundColor: "#e3e3e3",
+                                        height: "3px",
+                                    },
+                                    active: {
+                                        backgroundColor: "#333",
+                                        height: "3px",
+                                    },
+                                    thumb: {
+                                        marginTop: "-1.5px",
+                                        width: "10px",
+                                        height: "10px",
+                                        backgroundColor: "#333",
+                                        borderRadius: 50,
+                                    },
+                                }}
+                            />
+                            <span className="time2">{formatTime(duration)}</span>
+                        </div>
+                    </div>
                 </div>
-                <div
-                    className="Next-Button"
-                    onClick={() => setAudioIndex((audioIndex + 1) % audios.length)}
-                >
-                    <NextIcon />
-                </div>
-            </div>
-            <div className="timeSliderCustom">
-            <TimeSlider
-                axis="x"
-                xmax={duration}
-                x={currentTime}
-                onChange={handleTimeSliderChange}
-                styles={{
-                    track: {
-                        backgroundColor: "#e3e3e3",
-                        height: "2px",
-                    },
-                    active: {
-                        backgroundColor: "#333",
-                        height: "2px",
-                    },
-                    thumb: {
-                        marginTop: "-3px",
-                        width: "1px",
-                        height: "8px",
-                        backgroundColor: "#333",
-                        borderRadius: 0,
-                    },
-                }}
-            />
-            </div>
             </div>
             <audio
                 ref={audioRef}
