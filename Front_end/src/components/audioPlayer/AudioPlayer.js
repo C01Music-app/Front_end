930985b96aa587bba5632cb7acd93dd43cd81a8d
truongphan4898 <truongphan4898@gmail.com>
2024-07-20 00:15:47 +0700

    pullcode

--- Front_end/src/components/audioPlayer/AudioPlayer.js
+++ Front_end/src/components/audioPlayer/AudioPlayer.js
@@ -96,7 +96,7 @@ const AudioPlayer = () => {
     }
   };
 
-  const handleVolumeChange = ({ x }) => {
+  const handleVolumeChange = ({x}) => {
     const newVolume = Math.min(Math.max(x, 0), 1);
     setVolume(newVolume);
     if (audioRef.current) {
@@ -158,52 +158,48 @@ const AudioPlayer = () => {
   };
 
   return (
-    <div className="">
-      <div className="container-audio col-12">
-        <div className="song-info col-3">
-          {songs[audioIndex] && (
-            <>
-              <h2 className="Song-Title">{songs[audioIndex].title}</h2>
-              <p className="Singer">{songs[audioIndex].artist[0].name}</p>
-            </>
-          )}
-        </div>
-        <div className="song-control col-6">
-          <div className="Control-Button-Group">
-            <div
-              className={`Random-Button ${isRandom ? "active" : ""}`}
-              onClick={handleRandomClick}
-            >
-              <FaRandom />
-            </div>
-            <div
-              className="Prev-Button"
-              onClick={() =>
-                dispatch(
-                  selectIndex((audioIndex - 1 + songs.length) % songs.length)
-                )
-              }
-            >
-              <FaBackward />
-            </div>
-            <div className="Pause-Play-Button" onClick={handlePausePlayClick}>
-              {isPlay ? <FaPause /> : <FaPlay />}
-            </div>
-            <div
-              className="Next-Button"
-              onClick={() =>
-                dispatch(selectIndex((audioIndex + 1) % songs.length))
-              }
-            >
-              <FaForward />
-            </div>
-            <div
-              className={`Redo-Button ${isRedo ? "active" : ""}`}
-              onClick={handleRedoClick}
-            >
-              <FaRedo />
-            </div>
+      <div className="">
+        <div className="container-audio col-12">
+          <div className="song-info col-3">
+            {songs[audioIndex] && (
+                <>
+                  <h2 className="Song-Title">{songs[audioIndex].title}</h2>
+                  <p className="Singer">{songs[audioIndex].artist}</p>
+                </>
+            )}
           </div>
+          <div className="song-control col-6">
+            <div className="Control-Button-Group">
+              <div
+                  className={`Random-Button ${isRandom ? "active" : ""}`}
+                  onClick={handleRandomClick}
+              >
+                <FaRandom/>
+              </div>
+              <div
+                  className="Prev-Button"
+                  onClick={() =>
+                      dispatch(selectIndex((audioIndex - 1 + songs.length) % songs.length))
+                  }
+              >
+                <FaBackward/>
+              </div>
+              <div className="Pause-Play-Button" onClick={handlePausePlayClick}>
+                {isPlay ? <FaPause/> : <FaPlay/>}
+              </div>
+              <div
+                  className="Next-Button"
+                  onClick={() => dispatch(selectIndex((audioIndex + 1) % songs.length))}
+              >
+                <FaForward/>
+              </div>
+              <div
+                  className={`Redo-Button ${isRedo ? "active" : ""}`}
+                  onClick={handleRedoClick}
+              >
+                <FaRedo/>
+              </div>
+            </div>
 
             <div className="timeSliderCustom">
               <div className="time-display">
